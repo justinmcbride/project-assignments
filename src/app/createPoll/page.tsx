833ae0d645edc6f9ff7ed28bf6911b3d8bd4a980
Justin McBride <justin@mcbride.io>
2023-12-02 18:04:41 -0700

    first drop target

---
+++ src/app/createPoll/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+      <h1 className="text-4xl font-bold">Create a Poll</h1>
+    </main>
+  )
+}
