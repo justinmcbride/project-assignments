5e1627c69d5e8f936dd61d33dc8e066c4ac4441a
Justin McBride <justin@mcbride.io>
2023-12-04 16:44:37 -0700

    meta changes

--- src/app/createPoll/page.tsx
+++
@@ -1,9 +0,0 @@
-const CreatePoll = () => {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Create a Poll</h1>
-    </main>
-  );
-};
-
-export default CreatePoll;
